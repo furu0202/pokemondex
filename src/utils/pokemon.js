2d4b24b856e0196c57e94ccfa9d07775a4f726d6
furu0202 <furuka.0202@gmail.com>
2023-07-01 22:28:37 +0900

    やり直しするため記述をリセット

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,17 +1,1 @@
-export const getAllPokemon = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => resolve(data));
-  });
-};
 
-export const getPokemon = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        resolve(data);
-      });
-  });
-};
