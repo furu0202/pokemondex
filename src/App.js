f0bfe7c7684812c6d73eb9812194e898baeefb6b
furu0202 <furuka.0202@gmail.com>
2023-07-01 23:45:22 +0900

    Pokemon API のデータを取得

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon.js';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全ポケモンのデータを取得
+      let res = await getAllPokemon(initialURL);
+    };
+  });
   return <div className='App'></div>;
 }
 
